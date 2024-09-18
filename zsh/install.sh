7c54f82181f237a63be08736275656418a72c2ef
Samuel Huff <samuel.huff@ayahealthcare.com>
2024-09-18 11:21:05 -0700

    fix: clean up gitconfig
    feature: configure wezterm
    
    feature: configure p10k

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,26 +3,22 @@
 # oh-my-zsh
 #
 # This installs OH-MY-ZSH and the p10k theme
-info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
 
-export ZSH=$HOME/.dotfiles
-if [ ! -d "$ZSH/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else 
-  info "OMZ already installed, skipping"
+if [ ! -d "$HOME/.dotfiles/.oh-my-zsh" ]; then
+  ZSH=$HOME/.dotfiles/.oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 fi
 
 # Install powerlevel10k
-if [ ! -d "$ZSH/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+if [ ! -d "$HOME/.dotfiles/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
   # Assume that if p10k isn't installed, the fonts aren't either
   git clone --depth=1 https://github.com/romkatv/powerlevel10k-media.git $HOME/downloads/powerlevel10k-media
   cp $HOME/downloads/powerlevel10k-media/*.ttf $HOME/Library/Fonts
+
+  # TODO: Need to patch the fonts inside the terminals. Since this script only sets up wezterm, that's the only one
+
   # And clean up
   rm -rf $HOME/downloads/powerlevel10k-media
 
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k
-else
-  info "P10k already installed, skipping"
 fi
