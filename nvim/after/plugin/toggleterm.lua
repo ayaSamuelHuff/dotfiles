2fa056ad1124b82f087d9d2510bb62045f10bb70
Samuel Huff <samuel.huff@ayahealthcare.com>
2024-09-18 16:00:39 -0700

    feature: improve which-key docs

---
+++ nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,5 @@
+local wk = require('which-key')
+
+wk.add({
+    {"<C-n>", desc = "Terminal"}
+})
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,13 +1,22 @@
-local M = {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+      keys = {
+        scroll_down = "<c-j>",
+        scroll_up = "<c-k>",
+      },
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
     },
-    config = function(_, opts)
-        local wk = require("which-key")
-        wk.setup(opts)
-    end,
+  },
 }
-
-return M
