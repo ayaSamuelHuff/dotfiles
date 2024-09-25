71bbda52e095a1e9fa27c74b5d5b2ffa88993cf6
Samuel Huff <samuel.huff@ayahealthcare.com>
2024-09-25 11:32:51 -0700

    feature: remove autosession
    feature: add file tree

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -1,8 +1,8 @@
 -- Change leader to space
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
+vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeToggle)
 -- Move lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
